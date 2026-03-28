45cecf9aa484610eb95bb315771f99cbf4bacdd7
xdwarf <m.danielsen@me.com>
2026-03-29 00:51:18 +0100

    moved and created own docker for emulator

--- emulator/vite.config.js
+++ emulator/vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/emulator/',
   logLevel: 'error', // Suppress warnings, only show errors
   plugins: [
     base44({
